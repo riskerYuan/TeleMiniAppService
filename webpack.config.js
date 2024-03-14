b208fddb9c3757ac739affbf8169c6f89bb46f64
m3360202 <403028240@qq.com>
2024-03-14 18:06:48 +0800

    fit vercel

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,50 @@
 const path = require('path');
-
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 module.exports = {
-  target: 'webworker',
-  mode: 'production',
-  entry: './index.js',
+  // 入口点，这里假设你的前端入口文件是 src/client/index.js
+  entry: path.resolve(__dirname, 'index.js'),
+  // 输出配置，这里假设你希望将打包后的文件输出到 public/dist 目录
   output: {
-    filename: 'worker.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
   },
-  optimization: {
-    minimize: true,
-  },
+  // 模块加载器配置
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
+        // 使用 babel-loader 转译 JavaScript 文件
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'] // 这个预设会根据你的目标环境，自动决定应用哪些转换/插件
-          }
-        }
-      }
-    ]
-  }
+        },
+      },
+      {
+        // 使用 style-loader 和 css-loader 处理 CSS 文件
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // 使用 file-loader 处理图片等静态资源
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      // 您可以根据需要添加更多规则来处理其他类型的文件
+    ],
+  },
+  // 插件配置，例如提供环境变量等
+  plugins: [
+    // ... 其他插件
+    new NodePolyfillPlugin(),
+  ],
+  // 开发工具配置，生产环境通常不需要 source maps
+  devtool: 'source-map',
+  // 其他配置...
 };
